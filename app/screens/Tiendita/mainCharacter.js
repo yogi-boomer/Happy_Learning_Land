3c7837bdfc2626e81930cf17fbc4e2538397cd01
yogi-boomer <61360360+yogi-boomer@users.noreply.github.com>
2020-12-15 17:40:37 -0600

    nuevas librerias

--- app/screens/Tiendita/mainCharacter.js
+++ app/screens/Tiendita/mainCharacter.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { View, Text, StyleSheet } from "react-native"
 
+
 export default class MainCharacter extends Component {
     render() {
         return (
